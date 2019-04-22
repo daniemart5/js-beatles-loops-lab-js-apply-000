81cfbade3848f47ae0b040799726662b6f6b2fcf
Danielle Martinoli <daniechris5@gmail.com>
2019-04-22 16:15:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+    document.write(musicans + "play" + instruments);
+
+theBeatlesPlay("John Lennon", guitar)
+theBeatlesPlay("Ringo Star", drums)
+theBeatlesPlay("Paul Mccartney", bass)
+theBeatlesPlay("George Harrison", lead)
+}
+
+function johnLennonFacts() {
+    const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ]
+}
+
+function iLoveTheBeatles() {
+
+}
+
+var i=0;
+while (i<=15) {
+    document.write(i + "<br />");
+    i++;
+}
