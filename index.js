776479d5ba059fd818a95183f9eceb5182cf8dfb
Danielle Martinoli <daniechris5@gmail.com>
2019-04-22 16:17:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+    return (musicans + "play" + instruments);
+}
+
+function johnLennonFacts() {
+    const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ]
+}
+
+function iLoveTheBeatles() {
+
+}
+
+var i=0;
+while (i<=15) {
+    document.write(i + "<br />");
+    i++;
+}
